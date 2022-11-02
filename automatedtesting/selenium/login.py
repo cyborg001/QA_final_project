1cb0d0c33b13f2b65aff09deef01b460edf0a7f4
cyborg001 <cgrs27@gmail.com>
2022-11-02 19:06:34 -0400

    preparaondo el resultado final, actualizando selenium test to output mylog.txt

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,9 +1,7 @@
-# # #!/usr/bin/env python
 from webbrowser import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
-
+from myLogger import Logger
 # # Start the browser and login with standard_user
 # def login (user, password):
 #     print ('Starting the browser...')
@@ -31,6 +29,8 @@ from selenium.webdriver.support.expected_conditions import presence_of_all_eleme
 # Start the browser and login with standard_user
 # def login(user, password):
 def login(user,password):
+    a = Logger()
+    a.setLog('info','****Iniciando el log*****')
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
@@ -41,6 +41,8 @@ def login(user,password):
     url =  'https://www.saucedemo.com'
     # url = 'http://automationpractice.com/index.php'
     print ('Browser started successfully. Navigating to the demo page to login.')
+    a.setLog('info','Browser started successfully. Navigating to the demo page to login.')
+    # a.setLog('info',f'{url}') //da un error al poner una url
     driver.get(url)
     # driver.implicitly_wait(1.5)
     title = driver.title
@@ -48,27 +50,38 @@ def login(user,password):
     driver.implicitly_wait(10.0)
     inventory = []
     print(f'The user {user} is logging')
+    a.setLog('info',f'The user {user} is logging')
+
     driver.find_element(by=By.CSS_SELECTOR,value='input[id="user-name"]').send_keys(user)
     print(f'Writing the password')
+    a.setLog('info',f'Writing the password')
+
     driver.find_element(by=By.CSS_SELECTOR,value='input[id="password"]').send_keys(password)
     print('Clicking in login button')
+    a.setLog('info','Clicking in login button')
+
     driver.find_element(by=By.CSS_SELECTOR,value='input[id="login-button"]').click()
     assert 'https://www.saucedemo.com/inventory.html' == driver.current_url
     # print(driver.find_element(by=By.CSS_SELECTOR,value='a[id="logout_sidebar_link"]'))
     print('Logged successfully!')
+    a.setLog('info','Logged successfully!')
+
     print(driver.current_url)
-    compras(driver)
+    compras(driver,a)
     return driver
 
-def compras(driver):
+def compras(driver,a):
+    
     inventory = []
     elementos = driver.find_elements(by=By.CLASS_NAME,value='btn.btn_primary.btn_small.btn_inventory')
     cantidad = len(elementos)
     elem = driver.find_elements(by=By.CLASS_NAME,value='inventory_item_name')
     print(f'There are {cantidad} products to buy.')
+    a.setLog('info',f'There are {cantidad} products to buy.')
     for n in range(cantidad):
         if 'ADD' in elementos[n].text:
             print(f'Product ({elem[n].text}) added to cart.')
+            a.setLog('info',f'Product ({elem[n].text}) added to cart.')
             elementos[n].click()
             inventory.append(n)
 
@@ -78,23 +91,35 @@ def compras(driver):
 
     badge = driver.find_element(by=By.ID,value='shopping_cart_container')
     if int(badge.text) == cantidad:
-        print('All products added to cart')
-    print('----------------------------------------------')
-    print('Removing products from cart')
+        mensaje = '''All products add to cart
+-----------------------------------------------
+Removing products from cart'''
+        a.setLog('info','removing products from cart')
+        print(mensaje)
+
+
+    #     print('All products added to cart')
+    # print('----------------------------------------------')
+    # print('Removing products from cart')
     elementos = driver.find_elements(by=By.CLASS_NAME,value='btn.btn_secondary.btn_small.btn_inventory')
 
     print(elementos[0].text)
     for n in range(cantidad):
         # if 'ADD' not in n.text:
         print(f'Removing ({elem[n].text}) product from cart')
+        a.setLog('info',f'Removing ({elem[n].text}) product from cart')
         elementos[n].click()
         # inventory.pop()
     print(badge.text,'---------------------')
+    a.setLog('info',f"{badge.text}-------------------")
     assert badge.text == ''
     if badge.text == '':
-        print('All products has been removed')
-        print('UI test has been passed!')
+        message = '''All products has been remove
+UI test has been passed!'''
+        print(message)
+        a.setLog('info','All product has been remove. UI test passed!')
     
     
     return driver
 driver = login('standard_user', 'secret_sauce')
+
